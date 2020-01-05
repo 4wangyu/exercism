8fd9178d9ff9b819ab2cb3e1e62708d2f9282b95
4wangyu <4wangyu@gmail.com>
2020-01-05 16:16:27 +0800

    dart:resistor_color

---
+++ dart/resistor-color/lib/resistor_color.dart
@@ -0,0 +1,17 @@
+class ResistorColor {
+  // Put your code here
+  List<String> colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  int colorCode(String s) => this.colors.indexOf(s);
+}
