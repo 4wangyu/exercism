48e36045f53fa7d59ff6cdf8dce8f52aa35a4401
4wangyu <4wangyu@gmail.com>
2020-02-06 17:30:33 +0800

    fix capital letter

--- dart/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,15 +1,15 @@
 class ResistorColorDuo {
   var map = {
-    'Black': 0,
-    'Brown': 1,
-    'Red': 2,
-    'Orange': 3,
-    'Yellow': 4,
-    'Green': 5,
-    'Blue': 6,
-    'Violet': 7,
-    'Grey': 8,
-    'White': 9
+    'black': '0',
+    'brown': '1',
+    'red': '2',
+    'orange': '3',
+    'yellow': '4',
+    'green': '5',
+    'blue': '6',
+    'violet': '7',
+    'grey': '8',
+    'white': '9'
   };
 
   int value(List<String> list) {
