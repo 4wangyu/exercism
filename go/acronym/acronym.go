df683a162a3bc2fe3b6906889ace4c8da3ab72d3
4wangyu <4wangyu@gmail.com>
2021-02-03 23:16:41 +0800

    go/acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,23 @@
+package acronym
+
+import "strings"
+
+// Abbreviate returns acronym.
+func Abbreviate(s string) (out string) {
+	s = replaceWithSpace(s, []string{"-", "_"})
+
+	words := strings.Fields(s)
+
+	for i := range words {
+		out += string(words[i][0])
+	}
+
+	return strings.ToUpper(out)
+}
+
+func replaceWithSpace(s string, chars []string) string {
+	for i := range chars {
+		s = strings.ReplaceAll(s, chars[i], " ")
+	}
+	return s
+}
